32211aa8a3efb0099b7b9e9e119acb4a280b2195
Lexan E Flowers <lexar5657@gmail.com>
2021-03-17 14:59:12 -0600

    login

---
+++ checkLogin.js
@@ -0,0 +1,20 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+let key = getCookie("token");
+if (key == null) {
+    console.log("log out");
+}
+console.log("login");
